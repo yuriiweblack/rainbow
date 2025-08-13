cdd754c6dbb13aedf1bd87249b38b9f7f298e016
yuriiweblack <yuriiwebblack@gmail.com>
2025-08-13 23:51:22 +0300

    photo +

--- js/main.js
+++ js/main.js
@@ -76,16 +76,30 @@
 // });
 
 
-$('.slider-blog__inner').slick({
-    dots: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow"></button>',
-    responsive: [
-        {
-            breakpoint: 769,
-            settings: {
-                arrows: false
-            }
-        },
-    ]
+// $('.slider-blog__inner').slick({
+//     dots: true,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow"></button>',
+//     responsive: [
+//         {
+//             breakpoint: 769,
+//             settings: {
+//                 arrows: false
+//             }
+//         },
+//     ]
+// });
+
+
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+        "slideShow",
+        "thumbs",
+        "zoom",
+        "fullScreen",
+        "share",
+        "close"
+    ],
+    loop: false,
+    protect: true
 });
