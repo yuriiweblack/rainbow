570dcd6eb248c0c66781babea7d24819362c5cab
yuriiweblack <yuriiwebblack@gmail.com>
2025-08-17 09:26:23 +0300

    -adaptive+header

--- js/main.js
+++ js/main.js
@@ -103,3 +103,19 @@ $('[data-fancybox="gallery"]').fancybox({
     loop: false,
     protect: true
 });
+
+$(function () {
+
+
+
+    $('.menu__btn, .menu a').on('click', function () {
+
+        $('.header__top-inner').toggleClass('header__top-inner--active');
+
+
+    });
+
+
+    var mixer = mixitup('.portfolio__content');
+
+});
